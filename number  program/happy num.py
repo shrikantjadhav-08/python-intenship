f0c7453a16d84cc648d74e8560ac76751fc672bd
shrikantjadhav-08 <shrikant932543@gmail.com>
2026-07-23 18:23:31 +0530

    Create happy num.py

---
+++ number  program/happy num.py
@@ -0,0 +1,16 @@
+num=int(input("Enter any  number : "))
+print("Orignal Number : ",num)
+orignal=num
+while(num!=1 and num!=4):
+    sum=0
+    while(num!=0):
+        digit=num%10
+        sum=sum+(digit*digit)
+        num//=10
+
+    num=sum
+
+if(num==1):
+    print(orignal,"is  Happy")
+else:
+    print(orignal,"is Sad")
