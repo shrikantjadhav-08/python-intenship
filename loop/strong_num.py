261c1ba39a62e097d9d3fae1777a627133fa182c
shrikantjadhav-08 <shrikant932543@gmail.com>
2026-07-23 18:15:53 +0530

    Create strong_num.py

---
+++ loop/strong_num.py
@@ -0,0 +1,20 @@
+# 6.Strong Number 
+# Check whether number is Strong Number.
+
+num=int(input("Enter any number : "))
+print("orignal Number : ",num)
+orignal=num
+sum=0
+while(num!=0):
+    fact=1
+    digit=num%10
+    for i in range(1,digit+1):
+        fact*=i
+    sum+=fact
+    num=num//10
+
+print("Sum : ",sum)
+if(orignal==sum):
+    print(orignal,"is a strong number")
+else:
+    print(orignal,"is not a strong number")
